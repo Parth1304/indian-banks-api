fe1aa9312a2c2328f4fefc86b7e7078843eaafaa
Parth1304 <97118975+Parth1304@users.noreply.github.com>
2025-01-10 14:07:08 +0530

    deployment ready

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ db_name = os.getenv('DB_NAME')
 app = Flask(__name__)
 
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
